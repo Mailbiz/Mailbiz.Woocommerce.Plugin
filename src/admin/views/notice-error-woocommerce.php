7e53188fe79bdf676b7680efd2bd8138f87e8188
Derik Gehring <derikgehring@gmail.com>
2025-02-13 19:58:37 -0300

    chore: improve wording in woocommerce disabled error

--- src/admin/views/notice-error-woocommerce.php
+++ src/admin/views/notice-error-woocommerce.php
@@ -1,5 +1,5 @@
 <div class="notice notice-error">
   <p>
-    <strong>Mailbiz:</strong> Sua integração foi desativada. Por favor, certifique-se que o plugin do WooCommerce se encontra ativo para reativar.
+    <strong>Mailbiz:</strong> Sua integração foi desativada. Por favor, certifique-se que o plugin do WooCommerce se encontra instalado e ativo para reativar a integração.
   </p>
 </div>
