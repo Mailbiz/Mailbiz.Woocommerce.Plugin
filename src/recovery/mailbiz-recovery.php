ce4c55c550ab25501e464c82fe1e2e6fdcba1491
Derik Gehring <derikgehring@gmail.com>
2025-02-13 19:28:54 -0300

    fix: add sanitization to recovery; fix bug in which variation_id equals to product_id would go trough;

--- src/recovery/mailbiz-recovery.php
+++ src/recovery/mailbiz-recovery.php
@@ -41,7 +41,7 @@ class Recovery
       return;
     }
 
-    $recovery = json_decode(base64_decode($_GET['_mb_cr_']), true);
+    $recovery = json_decode(base64_decode(sanitize_text_field(wp_unslash($_GET['_mb_cr_']))), true);
     if (
       !$recovery ||
       !isset($recovery['c']) ||
@@ -54,17 +54,27 @@ class Recovery
     }
 
     foreach ($recovery['its'] as $item) {
-      $quantity = (int) $item[0];
-      $product_id = (int) $item[1];
+      $quantity_str = trim(sanitize_text_field($item[0]));
+      $quantity = is_numeric($quantity_str) ? (int) $quantity_str : null;
+
+      $product_id_str = trim(sanitize_text_field($item[1]));
+      $product_id = is_numeric($product_id_str) ? (int) $product_id_str : null;
+
       // $sku = $item[2];
-      $properties_json = $item[3];
+
+      $properties_json = trim(sanitize_text_field($item[3]));
 
       $properties = json_decode($properties_json, true);
+      $properties_variation_id_str = isset($properties['variation_id']) ? sanitize_text_field($properties['variation_id']) : null;
 
-      $valid_variation_id = isset($properties['variation_id']) && $properties['variation_id'] !== $product_id;
-      $variation_id = $valid_variation_id ? $properties['variation_id'] : 0;
+      $valid_variation_id = isset($properties_variation_id_str) && $properties_variation_id_str !== $product_id_str;
+      $variation_id_str = $valid_variation_id ? $properties_variation_id_str : 0;
 
-      $variation_id = (int) $variation_id;
+      $variation_id = is_numeric($variation_id_str) ? (int) $variation_id_str : null;
+
+      if ($quantity === null || $product_id === null || $variation_id === null) {
+        continue;
+      }
 
       WC()->cart->add_to_cart($product_id, $quantity, $variation_id);
     }
